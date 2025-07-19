444505544d68e9814d544cffd4cea7addcf63557
Syed Izhan Khilji <skhilji@ibs-b.hu>
2025-07-19 05:05:32 +0200

    Delete kiro Projects/Pinn POC directory

--- kiro Projects/Pinn POC/src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AV-PINO: Adaptive Variational Physics-Informed Neural Operator
-for Motor Fault Diagnosis and Control
-"""
-
-__version__ = "0.1.0"
-__author__ = "AV-PINO Research Team"
--- kiro Projects/Pinn POC/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Test package for AV-PINO system."""
